ea40d0af8b4f0ab39e19cb9b5f15431a305fbf16
Jack Rudenko <i@madappgang.com>
2018-09-10 10:40:21 +1000

    Create fake server that works with memory models only

---
+++ cmd/fake/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+//this server works only with memory storages and generated data
+//should be used for test and CI environments only
+func main() {
+	fmt.Println("Fake server started")
+}
--- jwt/token.go
+++ jwt/token.go
@@ -16,3 +16,6 @@ type Claims struct {
 	Foo string `json:"foo"`
 	jwt.StandardClaims
 }
+
+//how to use JWT tokens full example
+//https://github.com/dgrijalva/jwt-go/blob/master/cmd/jwt/app.go
