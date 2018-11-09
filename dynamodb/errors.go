eab45dd52f83ff6357218e8fc93df730d92d6651
Jack Rudenko <i@madappgang.com>
2018-11-09 18:14:06 +1100

    Implement DynamoDB backend support

--- dynamodb/errors.go
+++ dynamodb/errors.go
@@ -7,14 +7,6 @@ type Error string
 func (e Error) Error() string { return string(e) }
 
 const (
-	//ErrorNotFound general not found error
-	ErrorNotFound = Error("not found")
-	//ErrorWrongDataFormat wrong input data provided
-	ErrorWrongDataFormat = Error("wrong data format")
-	//ErrorNotImplemented requested feature is not implemented yet
-	ErrorNotImplemented = Error("Not implemented")
-	//ErrorUserExists user already exists
-	ErrorUserExists = Error("User is already exists")
 	//ErrorInternalError internal error
 	ErrorInternalError = Error("Internal error")
 )
---
+++ dynamodb/token_storage.go
@@ -0,0 +1,135 @@
+package dynamodb
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/madappgang/identifo/model"
+)
+
+const (
+	//TokensTableName is collection to store refresh tokens
+	TokensTableName = "RefreshTokens"
+)
+
+//NewTokenStorage creates mew dynamodb storage
+func NewTokenStorage(db *DB) (model.TokenStorage, error) {
+	ts := TokenStorage{}
+	ts.db = db
+	return &ts, nil
+
+}
+
+//TokenStorage is dynamodb token storage
+type TokenStorage struct {
+	db *DB
+}
+
+//ensureTable ensures app storage table is exists in database
+func (ts *TokenStorage) ensureTable() error {
+	exists, err := ts.db.isTableExists(TokensTableName)
+	if err != nil {
+		return err
+	}
+	if !exists {
+		//create table, AWS DynamoDB table creation is overcomplicated for sure
+		input := &dynamodb.CreateTableInput{
+			AttributeDefinitions: []*dynamodb.AttributeDefinition{
+				{
+					AttributeName: aws.String("token"),
+					AttributeType: aws.String("S"),
+				},
+			},
+			KeySchema: []*dynamodb.KeySchemaElement{
+				{
+					AttributeName: aws.String("token"),
+					KeyType:       aws.String("HASH"),
+				},
+			},
+			ProvisionedThroughput: &dynamodb.ProvisionedThroughput{
+				ReadCapacityUnits:  aws.Int64(10),
+				WriteCapacityUnits: aws.Int64(10),
+			},
+			TableName: aws.String(TokensTableName),
+		}
+		_, err = ts.db.C.CreateTable(input)
+		return err
+	}
+	return nil
+}
+
+//SaveToken save token in database
+func (ts *TokenStorage) SaveToken(token string) error {
+	if len(token) == 0 {
+		return model.ErrorWrongDataFormat
+	}
+	if ts.HasToken(token) {
+		return nil
+	}
+
+	t := Token{Token: token}
+	tv, err := dynamodbattribute.MarshalMap(t)
+	if err != nil {
+		return ErrorInternalError
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      tv,
+		TableName: aws.String(TokensTableName),
+	}
+	_, err = ts.db.C.PutItem(input)
+	if err != nil {
+		return ErrorInternalError
+	}
+	return nil
+
+}
+
+//HasToken returns true if the token in the storage
+func (ts *TokenStorage) HasToken(token string) bool {
+	if len(token) == 0 {
+		return false
+	}
+
+	result, err := ts.db.C.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String(TokensTableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"token": {
+				S: aws.String(token),
+			},
+		},
+	})
+	if err != nil {
+		return false
+	}
+	//empty result
+	if result.Item == nil {
+		return false
+	}
+	return true
+}
+
+//RevokeToken removes token from the storage
+func (ts *TokenStorage) RevokeToken(token string) error {
+	if !ts.HasToken(token) {
+		return model.ErrorNotFound
+	}
+	_, err := ts.db.C.DeleteItem(&dynamodb.DeleteItemInput{
+		TableName: aws.String(TokensTableName),
+		Key: map[string]*dynamodb.AttributeValue{
+			"token": {
+				S: aws.String(token),
+			},
+		},
+	})
+	if err != nil {
+		return ErrorInternalError
+	}
+	return nil
+
+}
+
+//Token is struct to store tokens in database
+type Token struct {
+	Token string `json:"token,omitempty"`
+}
