252a8c6280401c5c6c8d8403e42f479b5f5048ce
Jack Rudenko <i@madappgang.com>
2018-12-13 16:21:59 +1100

    Implement Fake database service

--- cmd/fake/main.go
+++ cmd/fake/main.go
@@ -1,82 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/joho/godotenv"
-	ihttp "github.com/madappgang/identifo/http"
-	"github.com/madappgang/identifo/jwt"
-	"github.com/madappgang/identifo/mailgun"
-	"github.com/madappgang/identifo/mem"
-	"github.com/madappgang/identifo/model"
+	"github.com/madappgang/identifo/server/fake"
 )
-
-func staticPages() ihttp.StaticPages {
-	return ihttp.StaticPages{
-		Login:                 "../../static/login.html",
-		Registration:          "../../static/registration.html",
-		ForgotPassword:        "../../static/forgot-password.html",
-		ResetPassword:         "../../static/reset-password.html",
-		ForgotPasswordSuccess: "../../static/forgot-password-success.html",
-		TokenError:            "../../static/token-error.html",
-		ResetSuccess:          "../../static/reset-success.html",
-	}
-}
-
-func staticFiles() ihttp.StaticFiles {
-	return ihttp.StaticFiles{
-		StylesDirectory:  "../../static/css",
-		ScriptsDirectory: "../../static/js",
-	}
-}
 
 //this server works only with memory storages and generated data
 //should be used for test and CI environments only
 func main() {
-	fmt.Println("Fake server started")
 
-	appStorage := mem.NewAppStorage()
-	userStorage := mem.NewUserStorage()
-	tokenStorage := mem.NewTokenStorage()
+	settings := fake.DefaultSettings
+	settings.StaticFolderPath = "../.."
+	settings.PEMFolderPath = "../../jwt"
+	settings.Issuer = "http://localhost:8080"
 
-	app := mem.MakeAppData("59fd884d8f6b180001f5b4e2", "secret", true, "Test app", []string{"offline", "smartrun"}, true, "", 0, 0)
-	if _, err := appStorage.AddNewApp(app); err != nil {
-		panic(err)
-	}
-
-	tokenService, _ := jwt.NewTokenService(
-		"../../jwt/private.pem",
-		"../../jwt/public.pem",
-		"identifo.madappgang.com",
-		model.TokenServiceAlgorithmAuto,
-		tokenStorage,
-		appStorage,
-		userStorage,
-	)
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	domain := os.Getenv("MAILGUN_DOMAIN")
-	privateKey := os.Getenv("MAILGUN_PRIVATE_KEY")
-	publicKey := os.Getenv("MAILGUN_PUBLIC_KEY")
-	emailService := mailgun.NewEmailService(domain, privateKey, publicKey, "sender@identifo.com")
-
-	sp := staticPages()
-	sf := staticFiles()
-
-	r, err := ihttp.NewRouter(nil, appStorage, userStorage, tokenStorage, tokenService, emailService, ihttp.ServeStaticPages(sp), ihttp.ServeStaticFiles(sf))
-
+	server, err := fake.NewServer(settings)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	if err := http.ListenAndServe(":8080", server.Router()); err != nil {
 		panic(err)
 	}
 }
