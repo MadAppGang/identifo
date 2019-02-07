d96f649032d6ae9856180c1cedda711917ca3eef
Mark Salabutin <salabutinmark4work@gmail.com>
2019-02-07 07:45:37 +0200

    Remove unused code

--- cmd/fake/main.go
+++ cmd/fake/main.go
@@ -8,25 +8,6 @@ import (
 	"github.com/madappgang/identifo/server/fake"
 )
 
-func staticPages() ihttp.StaticPages {
-	return ihttp.StaticPages{
-		Login:                 "../../static/login.html",
-		Registration:          "../../static/registration.html",
-		ForgotPassword:        "../../static/forgot-password.html",
-		ResetPassword:         "../../static/reset-password.html",
-		ForgotPasswordSuccess: "../../static/forgot-password-success.html",
-		TokenError:            "../../static/token-error.html",
-		ResetSuccess:          "../../static/reset-success.html",
-	}
-}
-
-func staticFiles() ihttp.StaticFiles {
-	return ihttp.StaticFiles{
-		StylesDirectory:  "../../static/css",
-		ScriptsDirectory: "../../static/js",
-	}
-}
-
 //this server works only with memory storages and generated data
 //should be used for test and CI environments only
 func main() {
