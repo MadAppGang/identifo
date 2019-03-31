ae0dc40ab7ee455ffed8372eb838e9e03ae0b5fd
Mark Salabutin <salabutinmark4work@gmail.com>
2019-03-31 16:57:35 +0300

    Implement generic storage provider for the mongo package

--- server/mgo/composer.go
+++ server/mgo/composer.go
@@ -34,17 +34,17 @@ func (dc *DatabaseComposer) Compose() (
 	if err != nil {
 		return nil, nil, nil, nil, err
 	}
-	appStorage, err := mongo.NewAppStorage(db)
+	appStorage, err := dc.settings.StorageProvider.NewAppStorage(db)
 	if err != nil {
 		return nil, nil, nil, nil, err
 	}
 
-	userStorage, err := mongo.NewUserStorage(db)
+	userStorage, err := dc.settings.StorageProvider.NewUserStorage(db)
 	if err != nil {
 		return nil, nil, nil, nil, err
 	}
 
-	tokenStorage, err := mongo.NewTokenStorage(db)
+	tokenStorage, err := dc.settings.StorageProvider.NewTokenStorage(db)
 	if err != nil {
 		return nil, nil, nil, nil, err
 	}
--- server/mgo/server.go
+++ server/mgo/server.go
@@ -2,21 +2,37 @@ package mgo
 
 import (
 	"github.com/madappgang/identifo/model"
+	"github.com/madappgang/identifo/mongo"
 	"github.com/madappgang/identifo/server"
 )
+
+// StorageProvider holds together all storage creators functions.
+type StorageProvider struct {
+	NewAppStorage   func(*mongo.DB) (model.AppStorage, error)
+	NewUserStorage  func(*mongo.DB) (model.UserStorage, error)
+	NewTokenStorage func(*mongo.DB) (model.TokenStorage, error)
+}
+
+var defaultSP = StorageProvider{
+	NewAppStorage:   mongo.NewAppStorage,
+	NewUserStorage:  mongo.NewUserStorage,
+	NewTokenStorage: mongo.NewTokenStorage,
+}
 
 // Settings are the extended settings for MongoDB server.
 type Settings struct {
 	model.ServerSettings
 	DBEndpoint string
 	DBName     string
+	StorageProvider
 }
 
 // DefaultSettings are default server settings.
 var DefaultSettings = Settings{
-	ServerSettings: server.DefaultSettings,
-	DBEndpoint:     "localhost:27017",
-	DBName:         "identifo",
+	ServerSettings:  server.DefaultSettings,
+	DBEndpoint:      "localhost:27017",
+	DBName:          "identifo",
+	StorageProvider: defaultSP,
 }
 
 // NewServer creates new backend service with MongoDB support.
