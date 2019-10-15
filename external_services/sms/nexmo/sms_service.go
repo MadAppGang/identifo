7b6dbc75d5e32587acef6390e050816932e0146c
Denis Ivanov <di@madappgang.com>
2019-10-15 10:37:13 +0300

    fix pull request issues

--- external_services/sms/nexmo/sms_service.go
+++ external_services/sms/nexmo/sms_service.go
@@ -2,32 +2,29 @@ package nexmo
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/madappgang/identifo/model"
 	"github.com/njern/gonexmo"
 )
 
-// SMSService sends SMS via Twilio service.
+// SMSService sends SMS via Nexmo service.
 type SMSService struct {
-	messagingServiceSid string
-	client              *nexmo.Client
+	client *nexmo.Client
 }
 
-// NewSMSService creates, inits and returns Twilio-backed SMS service.
+// NewSMSService creates, inits and returns Nexmo-backed SMS service.
 func NewSMSService(settings model.SMSServiceSettings) (*SMSService, error) {
 	client, err := nexmo.NewClient(settings.ApiKey, settings.ApiSecret)
 	if err != nil {
 		return nil, err
 	}
 	t := &SMSService{
-		messagingServiceSid: settings.ServiceSid,
-		client:              client,
+		client: client,
 	}
 	return t, nil
 }
 
-// SendSMS sends SMS messages using Twilio service.
+// SendSMS sends SMS messages using Nexmo service.
 func (ss *SMSService) SendSMS(recipient, message string) error {
 	if ss.client == nil {
 		return errors.New("Nexmo SMS service is not configured. ")
@@ -38,7 +35,16 @@ func (ss *SMSService) SendSMS(recipient, message string) error {
 		Type: nexmo.Text,
 		Text: message,
 	}
+
 	resp, err := ss.client.SMS.Send(msg)
-	fmt.Println(resp)
-	return err
+	if err != nil {
+		return err
+	}
+	for _, messageReport := range resp.Messages {
+		if messageReport.Status != nexmo.ResponseSuccess {
+			return errors.New(messageReport.ErrorText)
+		}
+	}
+
+	return nil
 }
