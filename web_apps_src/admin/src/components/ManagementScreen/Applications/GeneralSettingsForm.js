f0592ee1a3dcff43108a8e59768df9c9217e0358
nikita_kysil <pancxxx2@gmail.com>
2021-12-06 14:52:52 +0200

    fix tempo in 2fa status

--- web_apps_src/admin/src/components/ManagementScreen/Applications/GeneralSettingsForm.js
+++ web_apps_src/admin/src/components/ManagementScreen/Applications/GeneralSettingsForm.js
@@ -206,7 +206,7 @@ const ApplicationGeneralSettingsForm = (props) => {
             placeholder="Select TFA Status"
           >
             <Option value="disabled" title="Disabled" />
-            <Option value="mandaroty" title="Mandatory" />
+            <Option value="mandatory" title="Mandatory" />
             <Option value="optional" title="Optional" />
           </Select>
         </Field>
