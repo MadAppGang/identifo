1254cd3a35f6b13727d19b52afdbb31595660596
Jack Rudenko <mail@erudenko.com>
2022-11-02 21:49:14 +0100

    add tests for misocnfigured cors

---
+++ web/api/misconfig_test/cors_test.go
@@ -0,0 +1,25 @@
+package api_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCorsAlwaysTrue(t *testing.T) {
+	var message string
+	request.Post("/auth/login").
+		SetHeader("X-Identifo-ClientID", cfg.AppID).
+		Expect(t).
+		AssertFunc(validateJSON(func(data map[string]interface{}) error {
+			err := data["error"].(map[string]interface{})
+			message = err["detailed_message"].(string)
+			return nil
+		})).
+
+		// AssertFunc(dumpResponse).
+		Status(500).
+		Done()
+	assert.NotEmpty(t, message)
+	assert.Contains(t, message, "DefaultStorage settings: unsupported database type wrong_type")
+}
